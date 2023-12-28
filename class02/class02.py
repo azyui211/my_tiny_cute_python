c4fdfe1a877202f3a33026738c522fdeb04a80e2
Porcelain <azyui@naver.com>
2023-12-28 09:16:49 +0900

    add class02.py

---
+++ class02/class02.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+import os
+
+
+shot_name = "EDU_0010"
+
+file_name = "EDU_0010_ani01_v01.mov"
+split_name = file_name.split("_")
+sequence_name = split_name[0]
+shot_name = split_name[0] + "_" + split_name[1]
+shot_name.split("_")
+
+for i in range(5):
+    print(i)
+
+A = "AAA"
+A == "AAA"
+
+True False
+for i in ["A", "B", "C"]:
+    if i 
+    print(i)
+0 == False
+54 == True
+listA = ["Result"]
+
+if listA:
+    print("A")
+
+if status == "ip":
+    print("진행중")
+else:
+    print("진행 중이 아님")
+
+{"A": "B", "C":"D"}
+
+[{   
+}]
+
+print()
+
+list = ["A", "B"]
+tuple = ("A", "B")
