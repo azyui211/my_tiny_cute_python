41ab831f566405f4cfb652cfee7612e3e4566857
seokwon.choi <seokwon.choi@giantstepcorp.com>
2024-01-09 11:00:48 +0900

    update class05

--- class05/class05.py
+++ class05/class05.py
@@ -14,28 +14,20 @@
 
 import shotgun_api3
 
-class shotgrid_api():
-    def __init__(self):
-        self.sg = self.get_sg()
-        
-    def get_sg(self):
-        SERVER_PATH = 'https://giantstep.shotgunstudio.com'
-        SCRIPT_NAME = 'shotgrid_mov_uploader'
-        SCRIPT_KEY  = 'mvmxazyukt&dqgh2lbgBqpyyz'
-        sg = shotgun_api3.Shotgun(SERVER_PATH, SCRIPT_NAME, SCRIPT_KEY)
-
-        return sg
-
-import shotgun_api3
 SERVER_PATH = 'https://giantstep.shotgunstudio.com'
 SCRIPT_NAME = 'shotgrid_mov_uploader'
 SCRIPT_KEY  = 'mvmxazyukt&dqgh2lbgBqpyyz'
 
 sg = shotgun_api3.Shotgun(SERVER_PATH, SCRIPT_NAME, SCRIPT_KEY)
 
+# 샷그리드 검색 기본형
 필터 = []
-필드 목록 = []
+필드목록 = []
+sg.find("", 필터, 필드목록)
 
+
+# 샷그리드 검색 예제
+# Shot 페이지에서 APP_0010 샷의 시작 프레임을 가져오기
 filt = [["code", "is", "APP_0010"],
         ["project.Project.name", "is", "2023_06_theKillers"] ]
 fied = ["sg_cut_in"]
